a5325b2a17840bcc9c410fd3a30db35c7fbb22e7
Md.Jayed Gazi <mdjayedgazi@gmail.com>
2025-12-29 13:23:44 +0600

    nav section is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,7 @@ module.exports = {
     ],
     theme: {
         extend: {
-            fontFamily: {
-                pop: ["Poppins", "sans-serif"],
-            }
+
         },
     },
     plugins: [],
